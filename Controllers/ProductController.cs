24460d8b05ea4f71fa75460a62022fe897a698d1
Gvaurdin <gvaurdin777@yandex.ru>
2024-07-21 19:21:33 +0300

    Добавьте файлы проекта.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using HW_WebApplication_IntroductionToASP_NET.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW_WebApplication_IntroductionToASP_NET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private static List<Product> products =
+        [
+            new Product
+            {
+                Id = 1,
+                Title = "Car",
+                Description = "Audi 100",
+                Price = 500000,
+                Date = new DateTime(1998,1,15)
+            }
+        ];
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        [HttpPost]
+        public void CreateProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        [HttpPut]
+        public void UpdateProduct(int id,Product newProduct)
+        {
+            var updatedProduct = products.First(x=> x.Id == id);
+
+            updatedProduct.Title = newProduct.Title;
+            updatedProduct.Description = newProduct.Description;
+            updatedProduct.Price = newProduct.Price;
+            updatedProduct.Date = newProduct.Date;
+        }
+
+        [HttpDelete]
+
+        public void DeleteProduct(int id)
+        {
+            var deleteProduct = products.First(x => x.Id == id);
+            products.Remove(deleteProduct);
+        }
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace HW_WebApplication_IntroductionToASP_NET.Model
+{
+    public class Product
+    {
+        public required int Id { get; set; }
+        public required string Title { get; set; }
+
+        public required string Description { get; set; }
+
+        public required double Price{ get; set; }
+
+        public required DateTime Date { get; set; }
+    }
+}
